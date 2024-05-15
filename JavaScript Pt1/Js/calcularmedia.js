7da8821abac4d9ba1e6c5ca6f1188de4aa07485c
Ana Beatriz Vieira <151677391+Anaavieira@users.noreply.github.com>
2024-05-15 10:38:11 -0300

    Add files via upload
    Introdução a JavaScript no final do ano de 2023, pelo programa VisualCode

---
+++ JavaScript Pt1/Js/calcularmedia.js
@@ -0,0 +1,6 @@
+var n1 = parseFloat(window.prompt("Digite um número"))
+var n2 = parseFloat (window.prompt("Digite um número:"))
+var soma = n1+n2
+var media = soma/2
+document.write("<h1> A soma é:"+ soma + "<\h1>")
+document.write("<h1> A média é:"+ media + "<\h1>")
